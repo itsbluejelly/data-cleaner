385524a0de10b9eac1df026943f0797b01bc082a
itsbluejelly <ronniedenzel0@gmail.com>
2025-06-20 00:02:07 +0300

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""
+This package is meant to help interact with the data files of your choice.
+
+Currently the only supported files are:
+1. __.csv__: For comma-separated values or other delimiters
+2. __.text__: For normal text files
+"""
